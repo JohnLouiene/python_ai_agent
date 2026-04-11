01524c50895e35f0a056853a8edbf428afab248f
JohnLouiene <johnlouiene@gmail.com>
2026-04-11 20:20:29 +0800

    Added --verbose CLI argument

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ def main():
      
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
-    
+     
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     try:
@@ -32,10 +33,12 @@ def main():
     prompt_token_count = response.usage_metadata.prompt_token_count
     response_token_count = response.usage_metadata.candidates_token_count
 
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {prompt_token_count}")
-    print(f"Response tokens: {response_token_count}")
-    print(f"Response:\n{response.text}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {prompt_token_count}")
+        print(f"Response tokens: {response_token_count}")
+        print(f"Response:\n{response.text}") 
+    print(f"Response:\n{response.text}") 
 
 if __name__ == "__main__":
     main()
