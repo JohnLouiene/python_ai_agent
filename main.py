78b547f7e48ae33edcec841494ce9cff8e866b4e
JohnLouiene <johnlouiene@gmail.com>
2026-04-11 18:36:10 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+def main():
+    load_dotenv()
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if not api_key:
+        raise RuntimeError("API key does not exist")
+
+    client = genai.Client(api_key=api_key)
+     
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    try:
+        response = client.models.generate_content(
+            model='gemini-2.5-flash', contents= messages)
+    except Exception as e:
+        raise RuntimeError("API did not connect") from e
+
+    if response.usage_metadata == None:
+        raise RuntimeError("Usage metadata not received")
+
+    prompt_token_count = response.usage_metadata.prompt_token_count
+    response_token_count = response.usage_metadata.candidates_token_count
+
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {prompt_token_count}")
+    print(f"Response tokens: {response_token_count}")
+    print(f"Response:\n{response.text}")
+
+if __name__ == "__main__":
+    main()
